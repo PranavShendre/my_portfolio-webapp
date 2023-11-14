27f8d77502657ea81ab08384ee781f414d0b6a94
PranavShendre <pranavshendre08@gmail.com>
2023-11-14 18:29:35 +0530

    modified deployment

--- src/app/Components/HeroSection.jsx
+++ src/app/Components/HeroSection.jsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 
-
 const HeroSection = () => {
   return (
     <section className="lg:py-16">
@@ -17,7 +16,7 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hello, I'm{" "}
+              Hello, I&apos;m{" "}
             </span>
             <br></br>
             <TypeAnimation
